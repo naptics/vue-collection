ba5f303fb0b89e0291fa315d45b624c368a2009d
triumphvalley <timo.siegenthaler@napitcs.ch>
2022-06-09 17:12:18 +0200

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import { defineComponent } from 'vue'
+import './App.css'
 
 export default defineComponent({
     setup() {
         return () => (
             <div>
                 <header>
-                    <div class="wrapper">
+                    <div class="bg-red-500 text-2xl">
                         <HelloWorld message="Vue Collection" />
                     </div>
                 </header>

c8d72b78588f488d73e06aba177633206b493985
triumphvalley <timo.siegenthaler@naptics.ch>
2022-07-13 11:22:05 +0200

    improve button styling

--- src/components/base/NButton.tsx
+++ src/components/base/NButton.tsx
@@ -21,10 +21,10 @@ export default createComponent('NButton', nButtonProps, (props, { slots }) => {
             disabled={props.disabled}
             type={props.type}
             class={[
-                'font-medium rounded-md focus:outline-none focus-visible:ring-2 shadow ',
+                `font-medium rounded-md focus:outline-none focus-visible:ring-2 shadow text-${props.color}-900`,
                 !props.disabled
-                    ? `bg-${props.color}-200 hover:bg-${props.color}-300 text-${props.color}-900 focus-visible:ring-${props.color}-500`
-                    : `bg-${props.color}-100 text-${props.color}-200 cursor-default`,
+                    ? `bg-${props.color}-200 hover:bg-${props.color}-300 focus-visible:ring-${props.color}-500`
+                    : `bg-${props.color}-100 text-opacity-20 cursor-default`,
                 props.small ? 'py-1 px-2 text-xs' : 'py-2 px-4 text-sm',
             ]}
             onClick={props.onClick}

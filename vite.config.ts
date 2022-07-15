1fd13248a67efa0fa9ac8d87c74e229c95ede16e
triumphvalley <timo.siegenthaler@naptics.ch>
2022-07-15 12:20:49 +0200

    adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
     plugins: [vue(), vueJsx()],
     resolve: {
@@ -12,4 +13,5 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
+    base: '/libraries/vue-collection/',
 })

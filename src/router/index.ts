08f3d846753b2f8717cdd9cee12e7eac19bff709
Timo Siegenthaler <timo.siegenthaler@naptics.ch>
2023-03-22 11:07:33 +0100

    move views to demo folder

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,7 +13,7 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: () => import('../views/HomeView'),
+            component: () => import('../demo/views/HomeView'),
         },
     ],
 })

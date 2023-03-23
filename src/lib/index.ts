6974760812a9efdfe2b28d4e7ace2aad38508a1f
Timo Siegenthaler <timo.siegenthaler@naptics.ch>
2023-03-23 10:55:38 +0100

    add all components to index file

---
+++ src/lib/index.ts
@@ -0,0 +1,133 @@
+// All Components
+
+import NAlert, { nAlertProps, type AlertVariant as NAlertVariant } from './components/NAlert'
+export { NAlert, nAlertProps, NAlertVariant }
+
+import NBadge, { nBadgeProps } from './components/NBadge'
+export { NBadge, nBadgeProps }
+
+import NBreadcrumb, { nBreadcrumbProps, type BreadcrumbItem as NBreadcrumbItem } from './components/NBreadcrub'
+export { NBreadcrumb, nBreadcrumbProps, NBreadcrumbItem }
+
+import NButton, { nButtonProps } from './components/NButton'
+export { NButton, nButtonProps }
+
+import NCheckbox, { nCheckboxProps } from './components/NCheckbox'
+export { NCheckbox, nCheckboxProps }
+
+import NCheckboxLabel, { nCheckboxLabelProps } from './components/NCheckboxLabel'
+export { NCheckboxLabel, nCheckboxLabelProps }
+
+import NCrudModal, { nCrudModalProps } from './components/NCrudModal'
+export { NCrudModal, nCrudModalProps }
+
+import NDialog, { nDialogProps, type NDialogExposed, type DialogVariant as NDialogVariant } from './components/NDialog'
+export { NDialog, nDialogProps, NDialogExposed, NDialogVariant }
+
+import NDropdown, { nDropdownProps, type DropdownItem as NDropdownItem } from './components/NDropdown'
+export { NDropdown, nDropdownProps, NDropdownItem }
+
+import NDropzone, { nDropzoneProps } from './components/NDropzone'
+export { NDropzone, nDropzoneProps }
+
+import NForm, { nFormProps } from './components/NForm'
+export { NForm, nFormProps }
+
+import NFormModal, { nFormModalProps } from './components/NFormModal'
+export { NFormModal, nFormModalProps }
+
+import NIconButton, { nIconButtonProps } from './components/NIconButton'
+export { NIconButton, nIconButtonProps }
+
+import NInput, { nInputProps, type NInputExposed } from './components/NInput'
+export { NInput, nInputProps, NInputExposed }
+
+import NInputPhone, { nInputPhoneProps } from './components/NInputPhone'
+export { NInputPhone, nInputPhoneProps }
+
+import NInputSelect, {
+    nInputSelectProps,
+    type InputSelectOption as NInputSelectOption,
+} from './components/NInputSelect'
+export { NInputSelect, nInputSelectProps, NInputSelectOption }
+
+import NInputSuggestion, { nInputSuggestionProps } from './components/NInputSuggestion'
+export { NInputSuggestion, nInputSuggestionProps }
+
+import NLink, { nLinkProps } from './components/NLink'
+export { NLink, nLinkProps }
+
+import NList, { nListProps, type ListItem as NListItem } from './components/NList'
+export { NList, nListProps, NListItem }
+
+import NLoadingIndicator, { nLoadingIndicator } from './components/NLoadingIndicator'
+export { NLoadingIndicator, nLoadingIndicator }
+
+import NModal, { nModalProps, type ModalSlotProps as NModalSlotProps } from './components/NModal'
+export { NModal, nModalProps, NModalSlotProps }
+
+import NPagination, { nPaginationProps } from './components/NPagination'
+export { NPagination, nPaginationProps }
+
+import NSearchbar, { nSearchbarProps, type NSearchbarExposed } from './components/NSearchbar'
+export { NSearchbar, nSearchbarProps, NSearchbarExposed }
+
+import NSearchbarList, { nSearchbarListProps } from './components/NSearchbarList'
+export { NSearchbarList, nSearchbarListProps }
+
+import NSelect, { nSelectProps, type NSelectExposed } from './components/NSelect'
+export { NSelect, nSelectProps, NSelectExposed }
+
+import NSuggestionList, {
+    nSuggestionListProps,
+    nSuggestionListPropsForConfig,
+    nSuggestionListPropsForInput,
+    type InputSlotProps as NSuggestionListInputSlotProps,
+    type ItemSlotProps as NSuggestionListItemSlotProps,
+    type SuggestionItem as NSuggestionListItem,
+} from './components/NSuggestionList'
+export {
+    NSuggestionList,
+    nSuggestionListProps,
+    nSuggestionListPropsForConfig,
+    nSuggestionListPropsForInput,
+    NSuggestionListInputSlotProps,
+    NSuggestionListItemSlotProps,
+    NSuggestionListItem,
+}
+
+import NTable, {
+    nTableProps,
+    type TableHeading,
+    type TableDetail,
+    type TableItem,
+    type TableRow,
+} from './components/NTable'
+export { NTable, nTableProps, TableHeading, TableDetail, TableItem, TableRow }
+
+import NTableAction, { nTableActionProps } from './components/NTableAction'
+export { NTableAction, nTableActionProps }
+
+import NTextArea, { nTextAreaProps, type NTextAreaExposed } from './components/NTextArea'
+export { NTextArea, nTextAreaProps, NTextAreaExposed }
+
+import NTooltip, {
+    nTooltipProps,
+    nToolTipPropsForImplementor,
+    mapTooltipProps as mapNTooltipProps,
+    type TooltipPlacement as NTooltipPlacement,
+} from './components/NTooltip'
+export { NTooltip, nTooltipProps, nToolTipPropsForImplementor, mapNTooltipProps, NTooltipPlacement }
+
+import NValInput, {
+    nValInputProps,
+    validationProps as nValInputValidationProps,
+    type NValInputExposed,
+    type InputSlotProps as NValInputSlotProps,
+} from './components/NValInput'
+export { NValInput, nValInputProps, nValInputValidationProps, NValInputExposed, NValInputSlotProps }
+
+import { type ValidatedForm, createValidatedForm } from './components/ValidatedForm'
+export { ValidatedForm, createValidatedForm }
+
+// Utils

9a1e9fd1a046f0ce488e99800624b56d80ab9851
Timo Siegenthaler <timo.siegenthaler@naptics.ch>
2023-04-19 11:10:45 +0200

    add titleclass and textclass for nlist

--- src/lib/components/NList.tsx
+++ src/lib/components/NList.tsx
@@ -21,6 +21,14 @@ export const nListProps = {
         default: () => [],
     },
     /**
+     * Adds the classes to all title elements (on the left side).
+     */
+    titleClass: String,
+    /**
+     * Adds the classese to all text elements (on the right side).
+     */
+    textClass: String,
+    /**
      * Adds the classes to the top-level element.
      */
     addClass: String,
@@ -40,8 +48,10 @@ export default createComponent('NList', nListProps, props => {
                         index % 2 === 1 ? 'bg-white' : 'bg-default-50',
                     ]}
                 >
-                    <dt class="text-sm font-medium text-default-500">{buildElement(item.title)}</dt>
-                    <dd class="mt-1 text-sm sm:mt-0 sm:col-span-2">{buildElement(item.text)}</dd>
+                    <dt class={`text-sm font-medium text-default-500 ${props.titleClass}`}>
+                        {buildElement(item.title)}
+                    </dt>
+                    <dd class={`mt-1 text-sm sm:mt-0 sm:col-span-2 ${props.textClass}`}>{buildElement(item.text)}</dd>
                 </div>
             ))}
         </dl>
